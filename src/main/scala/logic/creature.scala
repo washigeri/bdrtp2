6ae718c413056d5471c78f4606716bea27e34782
Dieumi <oxeyo@hotmail.fr>
2018-04-01 18:31:07 -0400

    list creature + sort

--- src/main/scala/logic/creature.scala
+++ src/main/scala/logic/creature.scala
@@ -1,10 +1,10 @@
 package logic
-
+import scala.collection.mutable.ListBuffer
 /**
   * Created by oxeyo on 30/03/2018.
   */
 
-class creature(var name: String, var listspell: List[String]) {
+class creature(var name: String, var listspell: ListBuffer[String]=ListBuffer()) {
 
 
 }
--- src/main/scala/mainex1.scala
+++ src/main/scala/mainex1.scala
@@ -2,7 +2,9 @@ import net.ruippeixotog.scalascraper.browser.JsoupBrowser
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
 import net.ruippeixotog.scalascraper.dsl.DSL._
 import net.ruippeixotog.scalascraper.model._
-import logic.creature;
+import logic.creature
+
+import scala.collection.mutable.ListBuffer;
 
 object mainex1 {
 
@@ -16,10 +18,11 @@ object mainex1 {
   def main(args: Array[String]): Unit = {
     val res = BuildURLList()
     println(res)
-
+    var listCreature: ListBuffer[creature] = ListBuffer()
     for (k <- 0 to res.length-1) {
-      getSortByURL(res(k));
+      listCreature+= getSortByURL(res(k));
     }
+    print("test");
   }
 
   def BuildURLList(): List[String] = {
@@ -39,28 +42,35 @@ object mainex1 {
     res = uls.map(a => (if (id == 0) Base_URL + Bestiaries(id) else Root_URL) + (a >> attr("href")))
     res
   }
-  def getSortByURL(url: String): Unit  = {
+  def getSortByURL(url: String): creature  = {
     val res= HTTPQuery(url);
     val array= url.split("#");
-    val name = array(1);
+    val named = array(1);
     val tab= res.toString();
     val tabByCreatureBody = tab.split("<div class=\"body\">" );
 
     val tabByCreature= tabByCreatureBody(1).split("<h1");
-
+    val creature = new creature(name=named);
    for (k <- 0 to tabByCreature.length-1) {
-     if(tabByCreature(k).contains(name)){
+     if(tabByCreature(k).contains(named)){
 
         val tabspell=tabByCreature(k).split("/spells/");
         tabspell(0)="";
-        print("monster :"+name);
+        print("monster :"+named);
+
        print("\n")
         for(y <- 0 to tabspell.length-1){
-          print(tabspell(y).split(".html")(0));
-          print("\n")
+
+          if( tabspell(y).split(".html")(0)!=""){
+            print(tabspell(y).split(".html")(0));
+            creature.listspell+= tabspell(y).split(".html")(0).toString();
+            print("\n")
+          }
+
         }
      }
     }
+    creature
 
   }
 
