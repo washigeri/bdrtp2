3278b9202f6dedfc033e613fe3601451671edbb8
washigeri <malek.elouerghi@gmail.com>
2018-04-07 14:08:57 -0400

    fin ex1

--- src/main/scala/mainex1.scala
+++ src/main/scala/mainex1.scala
@@ -1,12 +1,15 @@
+import java.io.{File, IOException}
+import java.nio.file.{Files, Paths}
+
 import logic.creature
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
 import net.ruippeixotog.scalascraper.dsl.DSL._
 import net.ruippeixotog.scalascraper.model._
+import org.apache.commons.io.FileUtils
 import org.apache.spark.{SparkConf, SparkContext}
 
 import scala.collection.mutable.ListBuffer
-import scala.util.matching.Regex
 
 
 object mainex1 {
@@ -17,13 +20,10 @@ object mainex1 {
   val Indexes = Array("monsterIndex.html", "additionalMonsterIndex.html", "monsterIndex.html", "monsterIndex.html", "index.html")
   val Browser = JsoupBrowser()
   val MaxIndex = 4
-  val Conf : SparkConf = new SparkConf().setAppName("BDRTP2").setMaster("local[*]")
+  val Conf: SparkConf = new SparkConf().setAppName("BDRTP2").setMaster("local[*]")
 
   def main(args: Array[String]): Unit = {
     val res = BuildURLList()
-    val pattern =  new Regex("(\\w+)\\s(\\d+)");
-    val pattern(words,num) = "asas1 11"
-    // println(res)
     var listCreature: ListBuffer[creature] = ListBuffer()
     for (k <- res.indices) {
       listCreature += getSortByURL(res(k))
@@ -32,9 +32,12 @@ object mainex1 {
     sc.setLogLevel("ERROR")
     val distMonsters = sc.parallelize(listCreature)
     val pairs = distMonsters.flatMap(c => c.listspell.map(s => (s, c.name))).groupByKey()
+    val resultExists = Files.exists(Paths.get("./results"))
+    if (resultExists) {
+      DeleteDirectory("results")
+    }
     pairs.saveAsTextFile("results")
-    //val rescluster = pairs.collect()
-    //println(rescluster)
+
   }
 
   def BuildURLList(): List[String] = {
@@ -65,28 +68,21 @@ object mainex1 {
     val tabByCreature = tabByCreatureBody(1).split("<h1")
     val creature = new creature(name = named)
     for (k <- 0 until tabByCreature.length) {
-      val id=tabByCreature(k).split("class");
-      if (id(0).trim().contains("id=\""+named+"\"")) {
+      val id = tabByCreature(k).split("class")
+      if (id(0).trim().contains("id=\"" + named + "\"")) {
 
         val tabspell = tabByCreature(k).split("/spells/")
         tabspell(0) = ""
-        print("monster :" + named)
-
-        print("\n")
         for (y <- 0 until tabspell.length) {
 
           if (tabspell(y).split(".html")(0) != "") {
 
-            if(tabspell(y).split(".html")(0).contains("detectMagic#")){
-              val dm=tabspell(y).split(".html")(0).split("#");
-              print(dm(0))
-              creature.listspell+=dm(0);
-            }else{
-              print(tabspell(y).split(".html")(0))
+            if (tabspell(y).split(".html")(0).contains("detectMagic#")) {
+              val dm = tabspell(y).split(".html")(0).split("#")
+              creature.listspell += dm(0)
+            } else {
               creature.listspell += tabspell(y).split(".html")(0)
             }
-
-             print("\n")
           }
 
         }
@@ -99,5 +95,15 @@ object mainex1 {
   def HTTPQuery(url: String): Document = {
     val res = Browser.get(url)
     res
+  }
+
+  def DeleteDirectory(path: String): Unit = {
+    try {
+      FileUtils.deleteDirectory(new File(path))
+    } catch {
+      case ioe: IOException =>
+        // log the exception here
+        ioe.printStackTrace()
+    }
   }
 }
