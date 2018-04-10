d5de14b1f54862a4f68bb31e6874da7c99bcb323
washigeri <malek.elouerghi@gmail.com>
2018-04-10 11:30:00 -0400

    ajout worgrider & barbarian

--- src/main/scala/logic/Monster.scala
+++ src/main/scala/logic/Monster.scala
@@ -3,7 +3,8 @@ package logic
 trait Monster {
   var HP: Int
   var Armor: Int
-  var ListAction:List[String]
+  var ListAction: List[String]
+  var Speed: Int
 
-  def action{}
+  def action(distance: Int): String
 }
