118750f336c8bf9a216dbb6806cfc5cf541f2370
Dieumi <oxeyo@hotmail.fr>
2018-04-14 13:42:53 -0400

    classe node

---
+++ src/main/scala/logic/node.scala
@@ -0,0 +1,8 @@
+package logic
+
+/**
+  * Created by oxeyo on 14/04/2018.
+  */
+class node(val id : Int, val monster: Monster) {
+
+}
--- src/main/scala/mainex2.scala
+++ src/main/scala/mainex2.scala
@@ -5,7 +5,7 @@ object mainex2 {
   val Conf: SparkConf = new SparkConf().setAppName("BDRTP2ex2").setMaster("local[*]")
 
   def main(args: Array[String]): Unit = {
-
+      
   }
 
 }
