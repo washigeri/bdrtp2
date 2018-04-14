9141312067cdb74ba9489750aba37dd58aa5c250
Dieumi <oxeyo@hotmail.fr>
2018-04-14 13:59:32 -0400

    distance

--- src/main/scala/mainex2.scala
+++ src/main/scala/mainex2.scala
@@ -20,25 +20,25 @@ object mainex2 {
       (8L, new node(id = 8, monster = new Worgrider())), //H
       (9L, new node(id = 9, monster = new Worgrider())), //I
       (10L, new node(id = 10, monster = new Worgrider())),
-      (11L, new node(id = 1, monster = new Worgrider())), //A
-      (12L, new node(id = 2, monster = new BarbarianOrc())), //B
-      (13L, new node(id = 3, monster = new BarbarianOrc)), //C
-      (14L, new node(id = 4, monster = new BarbarianOrc)), //D
-      (15L, new node(id = 5, monster = new BarbarianOrc)), //E
-      (16L, new node(id = 6, monster = new Warlord()))//F
+      (11L, new node(id = 11, monster = new Worgrider())), //A
+      (12L, new node(id = 12, monster = new BarbarianOrc())), //B
+      (13L, new node(id = 13, monster = new BarbarianOrc)), //C
+      (14L, new node(id = 14, monster = new BarbarianOrc)), //D
+      (15L, new node(id = 15, monster = new BarbarianOrc)), //E
+      (16L, new node(id = 16, monster = new Warlord()))//F
     )) //J
 
 
     var myEdges = sc.makeRDD(Array(
-      Edge(1L, 2L, "2"), Edge(1L, 3L, "3"), Edge(1L, 4L, "4"),
-      Edge(1L, 5L, "5"), Edge(1L, 6L, "6"),
-      Edge(1L, 7L, "7"), Edge(1L, 8L, "8"),
-      Edge(1L, 9L, "9"), Edge(1L, 10L, "10"),
-      Edge(1L, 11L, "11"), Edge(1L, 12L, "12"),
-      Edge(1L, 13L, "13"),
-      Edge(1L, 14L, "14"),
-      Edge(1L, 15L, "15"),
-      Edge(1L, 16L, "16")
+      Edge(1L, 2L, "110"), Edge(1L, 3L, "110"), Edge(1L, 4L, "110"),
+      Edge(1L, 5L, "110"), Edge(1L, 6L, "110"),
+      Edge(1L, 7L, "110"), Edge(1L, 8L, "110"),
+      Edge(1L, 9L, "110"), Edge(1L, 10L, "110"),
+      Edge(1L, 11L, "110"), Edge(1L, 12L, "120"),
+      Edge(1L, 13L, "120"),
+      Edge(1L, 14L, "120"),
+      Edge(1L, 15L, "120"),
+      Edge(1L, 16L, "160")
     ))
   }
 
