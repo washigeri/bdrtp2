cb6ce58dabbe986e4106059f65ae9db5fa96c6da
washigeri <malek.elouerghi@gmail.com>
2018-04-23 11:14:30 -0400

    ajout dragon et movanicdeva

---
+++ src/main/scala/ex2/monsters/Dragon.scala
@@ -0,0 +1,31 @@
+package ex2.monsters
+
+import ex2.logic.MessageTypeEnum.MessageTypeEnum
+import ex2.logic.{MessageTypeEnum, Position}
+
+class Dragon(x: Double, y: Double) extends Serializable with Monster {
+  override var HP: Int = 391
+  override var Armor: Int = 37
+  override var ListAction: List[MessageTypeEnum] = List(MessageTypeEnum.MOVE, MessageTypeEnum.MELEE, MessageTypeEnum.RANGED)
+  override var Speed: Int = 40
+  override var maxHp: Int = 391
+  override var damageMelee: Damage = Damage(4, 8, 21)
+  override var damageRanged: Damage = Damage(24, 6, 0)
+  override var position: Position = new Position(x, y)
+  DamageReduction = 20
+  MeleeAtckCount = 1
+  RangedAtckCount = 3
+  RangedAtckChance = List(31, 31, 31)
+  MeleeAtckChance = List(33)
+  var flying = false
+
+  override def action(distance: Double): MessageTypeEnum = {
+    if (distance == 0 && !flying) {
+      ListAction(1)
+    }
+    else if (distance > 0 && flying && distance <= 70) {
+      ListAction(2)
+    }
+    ListAction.head
+  }
+}
